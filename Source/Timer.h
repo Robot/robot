1fba2a117501306f6eaa49b6f3f5309c8e2dd460
David Krutsko <dave@krutsko.net>
2015-05-08 05:47:13 -0400

    Implemented Timer Declarations

--- Source/Timer.h
+++ Source/Timer.h
@@ -27,6 +27,37 @@ namespace Robot {
 
 class ROBOT_EXPORT Timer
 {
+public:
+	Timer							(void);
+
+public:
+	void			Start			(void);
+	void			Reset			(void);
+	uint64			Restart			(void);
+
+	uint64			GetElapsed		(void) const;
+	bool			HasStarted		(void) const;
+	bool			HasExpired		(uint64 time) const;
+
+	static void		Sleep			(const Range& time);
+	static void		Sleep			(uint32 minimum,
+									 uint32 maximum);
+
+	static uint64	GetCpuTime		(void);
+
+public:
+	uint64			operator ()		(void) const;
+
+	bool			operator <		(const Timer& timer) const;
+	bool			operator >		(const Timer& timer) const;
+	bool			operator <=		(const Timer& timer) const;
+	bool			operator >=		(const Timer& timer) const;
+
+	bool			operator ==		(const Timer& timer) const;
+	bool			operator !=		(const Timer& timer) const;
+
+private:
+	uint64			mStarted;		// Time started (milliseconds)
 };
 
 }
