a733004b6f80e34210fcdc392d69354c292360a1
David Krutsko <dave@krutsko.net>
2015-07-21 12:11:07 -0400

    Implemented Clipboard Declarations

--- Source/Clipboard.cc
+++ Source/Clipboard.cc
@@ -12,8 +12,221 @@
 //----------------------------------------------------------------------------//
 
 #include "Clipboard.h"
+using std::string;
+
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+
+#endif
 ROBOT_NS_BEGIN
 
 
+
+//----------------------------------------------------------------------------//
+// Functions                                                        Clipboard //
+//----------------------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Clipboard::Clear (void)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Clipboard::HasText (void)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+string Clipboard::GetText (void)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return string();
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return string();
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return string();
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Clipboard::SetText (const char* text)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Clipboard::HasImage (void)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Clipboard::GetImage (Image& image)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Clipboard::SetImage (const Image& image)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+uint64 Clipboard::GetSequence (void)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return 0;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return 0;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return 0;
+
+#endif
+}
 
 ROBOT_NS_END
--- Source/Clipboard.h
+++ Source/Clipboard.h
@@ -15,6 +15,7 @@
 #define ROBOT_CLIPBOARD_H
 
 #include "Types.h"
+#include <string>
 ROBOT_NS_BEGIN
 
 
@@ -27,6 +28,28 @@ ROBOT_NS_BEGIN
 
 class ROBOT_EXPORT Clipboard
 {
+private:
+	 Clipboard							(void);
+	~Clipboard							(void);
+	 Clipboard							(const Clipboard&  clipboard);
+	 Clipboard							(      Clipboard&& clipboard);
+
+public:
+	static bool			Clear			(void);
+
+	static bool			HasText			(void);
+	static std::string	GetText			(void);
+	static bool			SetText			(const char* string);
+
+	static bool			HasImage		(void);
+	static bool			GetImage		(      Image& image);
+	static bool			SetImage		(const Image& image);
+
+	static uint64		GetSequence		(void);
+
+private:
+	Clipboard&			operator =		(const Clipboard&  clipboard);
+	Clipboard&			operator =		(      Clipboard&& clipboard);
 };
 
 ROBOT_NS_END
