725aa1c1fa736016177de798921a87500cdfa0d8
David Krutsko <dave@krutsko.net>
2015-07-21 12:12:27 -0400

    Implemented Screen Declarations

--- Source/Screen.cc
+++ Source/Screen.cc
@@ -12,8 +12,210 @@
 //----------------------------------------------------------------------------//
 
 #include "Screen.h"
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+
+#endif
 ROBOT_NS_BEGIN
 
 
+
+//----------------------------------------------------------------------------//
+// Locals                                                                     //
+//----------------------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////
+// Cannot be included as part of the Screen class due to the use of std::deque.
+
+static ScreenList gScreens;
+
+
+
+//----------------------------------------------------------------------------//
+// Fields                                                              Screen //
+//----------------------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////
+
+Bounds Screen::mTotalBounds;
+Bounds Screen::mTotalUsable;
+
+
+
+//----------------------------------------------------------------------------//
+// Constructors                                                        Screen //
+//----------------------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////
+
+Screen::Screen (void)
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+Screen::Screen (Bounds bounds, Bounds usable)
+{
+	mBounds = bounds;
+	mUsable = usable;
+}
+
+
+
+//----------------------------------------------------------------------------//
+// Functions                                                           Screen //
+//----------------------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////
+
+Bounds Screen::GetBounds   (void) const { return mBounds;                }
+Bounds Screen::GetUsable   (void) const { return mUsable;                }
+bool   Screen::IsPortrait  (void) const { return mBounds.W <  mBounds.H; }
+bool   Screen::IsLandscape (void) const { return mBounds.W >= mBounds.H; }
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Screen::Synchronize (void)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+Screen* Screen::GetMain (void)
+{
+	// TODO:
+	return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+ScreenList Screen::GetList (void)
+{
+	return gScreens;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+Screen* Screen::GetScreen (const Window& window)
+{
+	// TODO:
+	return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+Screen* Screen::GetScreen (const Point& point)
+{
+	// TODO:
+	return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+Screen* Screen::GetScreen (int32 px, int32 py)
+{
+	// TODO:
+	return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Screen::GrabScreen (Image& image, const Bounds&
+						 bounds, const Window& window)
+{
+	return GrabScreen (image,
+		  bounds.X, bounds.Y,
+		  bounds.W, bounds.H, window);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Screen::GrabScreen (Image& image,
+	int32 x, int32 y, int32 w, int32 h,
+	const Window& window)
+{
+#ifdef ROBOT_OS_LINUX
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_MAC
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+Bounds Screen::GetTotalBounds (void) { return mTotalBounds; }
+Bounds Screen::GetTotalUsable (void) { return mTotalUsable; }
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool Screen::IsCompositing (void)
+{
+#if defined (ROBOT_OS_MAC) || \
+	defined (ROBOT_OS_LINUX)
+
+	// TODO:
+	return false;
+
+#endif
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+	return false;
+
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void Screen::SetCompositing (bool enabled)
+{
+#ifdef ROBOT_OS_WIN
+
+	// TODO:
+
+#endif
+}
 
 ROBOT_NS_END
--- Source/Screen.h
+++ Source/Screen.h
@@ -15,7 +15,21 @@
 #define ROBOT_SCREEN_H
 
 #include "Types.h"
+#include "Window.h"
+#include <deque>
+
 ROBOT_NS_BEGIN
+	class Screen;
+
+
+
+//----------------------------------------------------------------------------//
+// Types                                                                      //
+//----------------------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef std::deque<Screen*> ScreenList;
 
 
 
@@ -27,6 +41,45 @@ ROBOT_NS_BEGIN
 
 class ROBOT_EXPORT Screen
 {
+public:
+	Screen								(void);
+	Screen								(Bounds bounds,
+										 Bounds usable);
+
+public:
+	Bounds				GetBounds		(void) const;
+	Bounds				GetUsable		(void) const;
+	bool				IsPortrait		(void) const;
+	bool				IsLandscape		(void) const;
+
+	static bool			Synchronize		(void);
+
+	static Screen*		GetMain			(void);
+	static ScreenList	GetList			(void);
+	static Screen*		GetScreen		(const Window& window);
+	static Screen*		GetScreen		(const Point & point );
+	static Screen*		GetScreen		(int32  px, int32  py);
+
+	static bool			GrabScreen		(Image& image, const Bounds& bounds,
+										 const Window& window = Window());
+
+	static bool			GrabScreen		(Image& image,
+										 int32 x =  0, int32 y =  0,
+										 int32 w = -1, int32 h = -1,
+										 const Window& window = Window());
+
+	static Bounds		GetTotalBounds	(void);
+	static Bounds		GetTotalUsable	(void);
+
+	static bool			 IsCompositing	(void);
+	static void			SetCompositing	(bool enabled);
+
+private:
+	Bounds				mBounds;		// Screen total  bounds
+	Bounds				mUsable;		// Screen usable bounds
+
+	static Bounds		mTotalBounds;	// System total  bounds
+	static Bounds		mTotalUsable;	// System usable bounds
 };
 
 ROBOT_NS_END
